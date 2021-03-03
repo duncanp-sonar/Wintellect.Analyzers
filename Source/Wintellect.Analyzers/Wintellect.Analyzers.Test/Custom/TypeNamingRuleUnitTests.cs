0881256e33fde34d3fad74f8da4fa8bf250c3f3d
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-03-03 14:51:42 +0000

    Add dummy custom rules to test Sonar plugin generation

---
+++ Source/Wintellect.Analyzers/Wintellect.Analyzers.Test/Custom/TypeNamingRuleUnitTests.cs
@@ -0,0 +1,74 @@
+﻿/*------------------------------------------------------------------------------
+Wintellect.Analyzers - .NET Compiler Platform ("Roslyn") Analyzers and CodeFixes
+Copyright (c) Wintellect. All rights reserved
+Licensed under the Apache License, Version 2.0
+See License.txt in the project root for license information
+------------------------------------------------------------------------------*/
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestHelper;
+
+namespace Wintellect.Analyzers.Test
+{
+    [TestClass]
+    public abstract class BaseTypeNamingUnitTests : DiagnosticVerifier
+    {
+        [TestMethod]
+        public void InvalidNameTest()
+        {
+            const String test = @"
+using System;
+
+namespace SomeTests
+{
+    public class XBasicClass
+    {
+    }
+}
+";
+            var descriptor = GetDescriptor();
+
+            var expected = new DiagnosticResult
+            {
+                Id = descriptor.Id,
+                Message = String.Format(descriptor.MessageFormat.ToString(), "XBasicClass"),
+                Severity = descriptor.DefaultSeverity,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 6, 18)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        protected abstract DiagnosticDescriptor GetDescriptor();
+    }
+
+    [TestClass]
+    public class TestRuleA : BaseTypeNamingUnitTests
+    {
+        protected override DiagnosticDescriptor GetDescriptor() => TypeNameRuleA.Rule;
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new TypeNameRuleA();
+    }
+
+
+    [TestClass]
+    public class TestRuleB : BaseTypeNamingUnitTests
+    {
+        protected override DiagnosticDescriptor GetDescriptor() => TypeNameRuleB.Rule;
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new TypeNameRuleB();
+    }
+
+    [TestClass]
+    public class TestRuleC : BaseTypeNamingUnitTests
+    {
+        protected override DiagnosticDescriptor GetDescriptor() => TypeNameRuleC.Rule;
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new TypeNameRuleC();
+    }
+}
---
+++ Source/Wintellect.Analyzers/Wintellect.Analyzers/Custom/TypeNamingRuleA.cs
@@ -0,0 +1,39 @@
+﻿/*------------------------------------------------------------------------------
+Wintellect.Analyzers - .NET Compiler Platform ("Roslyn") Analyzers and CodeFixes
+Copyright (c) Wintellect. All rights reserved
+Licensed under the Apache License, Version 2.0
+See License.txt in the project root for license information
+------------------------------------------------------------------------------*/
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Wintellect.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class TypeNameRuleA : DiagnosticAnalyzer
+    {
+        private const string CharToCheck = "A";
+
+        public static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor("SonarRoslyn_Type" + CharToCheck,
+            "Type names must start with " + CharToCheck,
+            "Invalid type: {0}. Type names must start with " + CharToCheck,
+            "Naming", DiagnosticSeverity.Warning, true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSymbolAction(CheckTypeName, SymbolKind.NamedType);
+        }
+
+        private void CheckTypeName(SymbolAnalysisContext context)
+        {
+            if(!context.Symbol.Name.StartsWith(CharToCheck))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(Rule, context.Symbol.Locations.FirstOrDefault(), context.Symbol.Name));
+            }
+        }
+    }
+}
